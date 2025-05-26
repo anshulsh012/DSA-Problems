535ee62999e21c110736836bdaca8684c0b0ecc5
Student Anshul <anshulsharma060205@gmail.com>
2025-05-26 14:34:00 +0530

    Leet Code Problem(Difficulty Easy) 66. Plus One

--- Easy/05_PlusOne.py
+++ Easy/05_PlusOne.py
@@ -8,12 +8,8 @@ class Solution:
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-            digits[i] = 0  # set current digit to 0 and continue
-
-        # If all digits were 9 (e.g., [9,9,9]), we now have [0,0,0]
-        # Add 1 at the beginning
+            digits[i] = 0
         return [1] + digits
-
 
 ans = Solution()
 
