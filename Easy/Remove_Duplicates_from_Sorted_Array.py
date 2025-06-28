f58ed7a21cbea7407601ca4f69b1e7b6c06d5e2b
anshulsh012 <anshulsh012@gmail.com>
2025-06-28 12:44:58 +0530

    DSA Easy Leet Code Problem 7

---
+++ Easy/Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,18 @@
+nums = [1,1,2,2]
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if not nums:
+            return []
+        
+        i = 0
+        for j in range(1, len(nums)):
+            if(nums[j] != nums[i]):
+                i+=1
+                nums[i] = nums[j]
+            
+        return i+1
+
+ans = Solution()
+
+print(ans.removeDuplicates(nums)) 
